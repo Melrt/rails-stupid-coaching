6696f6bb5c8ed7aa9c6275c6e77e903e6e04ed5a
melanie rat <RAT.melanie@gmail.com>
2019-03-11 14:47:34 +0100

    Challenge Stupid Coach with Rails

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,20 @@
+class QuestionsController < ApplicationController
+  def ask
+  end
+
+  def answer
+    @ask = params[:question]
+
+    if @ask == 'I am going to work' # => http://localhost:3000/answer?question=I+am+going+to+work
+      @answer = 'Great!'
+    elsif @ask.include?('?') # => http://localhost:3000/answer?question=%3F
+      # "Silly question, get dressed and go to work!"
+      @answer = 'Silly question, get dressed and go to work!'
+    else
+      @answer = "I don't care, get dressed and go to work!"
+    end
+  end
+end
+
+
+
